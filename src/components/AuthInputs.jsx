ff77e11263b56c3a7c964e72ba3ba6b65a86edd6
Vrushali Argade <vrushali.k.argade@gmail.com>
2025-08-02 22:23:31 +0530

    Tailwind CSS classes added

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,44 +1,46 @@
 import { useState } from "react";
-import { styled } from "styled-components";
-const ControlComponents = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-const Input = styled.input`
- width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color:${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")} ;
-  color::${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+//import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./Input";
+// const ControlComponents = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
+// const Label = styled.label`
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   font-size: 0.75rem;
+//   font-weight: 700;
+//   letter-spacing: 0.1em;
+//   text-transform: uppercase;
+//   color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
+// `;
+// const Input = styled.input`
+//  width: 100%;
+//   padding: 0.75rem 1rem;
+//   line-height: 1.5;
+//   background-color:${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")} ;
+//   color::${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+//   border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
+//   border-radius: 0.25rem;
+//   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+// `;
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
+//   &:hover {
+//     background-color: #f0920e;
+//   }
+//`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -53,6 +55,7 @@ export default function AuthInputs() {
   }
 
   function handleLogin() {
+    //alert();
     setSubmitted(true);
   }
 
@@ -60,41 +63,43 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlComponents>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col mb-6 gap-2 ">
         <p>
-          <Label
+          {/* <Label
             // className={`label ${emailNotValid ? 'invalid' : ''}`}
             $invalid={emailNotValid}
           >
             Email
-          </Label>
+          </Label> */}
           <Input
             type="email"
+            label="Email"
             // className={emailNotValid ? 'invalid' : undefined}
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label
-            // className={`label ${passwordNotValid ? 'invalid' : ''}`}
-            $invalid={passwordNotValid}
-          >
-            Password
-          </Label>
+          {/* <Label */}
+            {/* // className={`label ${passwordNotValid ? 'invalid' : ''}`} */}
+            {/* $invalid={passwordNotValid} */}
+          {/* > */}
+            {/* Password */}
+          {/* </Label> */}
           <Input
             type="password"
-            $invalid={passwordNotValid}
+            label="Password"
+            invalid={passwordNotValid}
             // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </ControlComponents>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({children,...props}){
+    return(
+        <button className="px-4 py-2 font-semibold rounded uppercase text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>{children}</button>
+    );
+}
